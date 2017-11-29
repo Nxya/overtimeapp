244425ae39b928fc815223c46769f8a674fb8ef1
Alex <alexb3030@gmail.com>
2017-11-29 12:02:18 -0700

    Employee homepage stylings

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,8 +1,18 @@
-@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow", phone: "8013686924")
+@user = User.create(email: "test@test.com",
+                    password: "asdfasdf",
+                    password_confirmation: "asdfasdf",
+                    first_name: "Jon",
+                    last_name: "Snow",
+                    phone: "4322386131")
 
-puts "1 user created"
+puts "1 User created"
 
-AdminUser.create(email: "admin@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Admin", last_name: "Name", phone: "8013686924")
+AdminUser.create(email: "admin@test.com",
+                  password: "asdfasdf",
+                  password_confirmation: "asdfasdf",
+                  first_name: "Admin",
+                  last_name: "Name",
+                  phone: "4322386131")
 
 puts "1 Admin User created"
 
@@ -12,8 +22,8 @@ end
 
 puts "100 Posts have been created"
 
-100.times do |audit_log|
-  AuditLog.create!(user_id: User.last.id, status: 0, start_date: (Date.today - 6.days))
-end
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 6.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 13.days))
+AuditLog.create!(user_id: @user.id, status: 0, start_date: (Date.today - 20.days))
 
-puts "100 audit logs have been created"
+puts "3 audit logs have been created"
