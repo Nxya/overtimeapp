86fa9d118357b986c68db56a4090307ac4591ae9
Alex <alexb3030@gmail.com>
2017-11-30 15:18:53 -0700

    Tried to fix dropdown, no luck

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,8 +11,8 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap-sprockets
 //= require gritter
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
+//= require bootstrap-sprockets
